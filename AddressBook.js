eaff98588c3b7bdb584f9b8320c880ef3f483278
Jay002 <jayesh2598@gmail.com>
2020-11-15 15:36:39 +0530

    Added: Functionality to edit an existing contact.

--- AddressBook.js
+++ AddressBook.js
@@ -4,7 +4,7 @@ const prompt = require('prompt-sync')();
 let addressBook = new Array();
 let choice = 0;
 do {
-    choice = prompt("Enter choice : 1. Add new contact 0. Exit ");
+    choice = prompt("Enter choice : 1. Add new contact 2. Edit contact 3. Print address book 0. Exit ");
     switch (choice) {
         case '1':
             let con = takeDetails();
@@ -12,6 +12,20 @@ do {
                 addressBook.push(con);
                 console.log("Contact added successfully!");
             }
+            break;
+        case '2':
+            if (addressBook.length == 0)
+                console.log("No contacts in address book.");
+            else {
+                let con = editContact();
+                if (con == 1)
+                    console.log("Changes applied successfully.");
+                else
+                    console.log("No such contact available.");
+            }
+            break;
+        case '3':
+            addressBook.forEach(contact => console.log(contact.toString()));
             break;
         case '0':
             console.log("Thanks for using the application.");
@@ -23,7 +37,7 @@ do {
 function takeDetails() {
     let fName = prompt("Enter firstName : ");
     let lName = prompt("Enter lastName : ");
-    let address = prompt("Enter address :");
+    let address = prompt("Enter address : ");
     let city = prompt("Enter city : ");
     let state = prompt("Enter state : ");
     let zip = prompt("Enter zip : ");
@@ -37,4 +51,64 @@ function takeDetails() {
         console.error(e);
     }
     return contact;
+}
+
+function editContact() {
+    let fName = prompt("Enter firstName of contact to edit : ");
+    let lName = prompt("Enter lastName of contact to edit: ");
+    let contact = addressBook.find((contact) => contact.firstName == fName && contact.lastName == lName);
+    if (contact == undefined)
+        return 0;
+    console.log("Which fields do you want to change?");
+    console.log("1.First Name \t2.Last Name \t3.Address \t4.City \n5.State \t6.Zip \t7.Phone number \t8.Email");
+    let more = true;
+    let changeFieldsArr = new Array();
+    do {
+        let number = prompt();
+        if (number >= 1 && number <= 8)
+            changeFieldsArr.push(number);
+        let moreProps = prompt("More? (y/n)");
+        if (moreProps == 'n')
+            more = false;
+    }
+    while (more == true);
+    for (let number = 0; number < changeFieldsArr.length; number++) {
+        switch (changeFieldsArr[number]) {
+            case '1':
+                let newFName = prompt("Enter new First Name : ");
+                contact.firstName = newFName;
+                break;
+            case '2':
+                let newLName = prompt("Enter new Last Name : ");
+                contact.lastName = newLName;
+                break;
+            case '3':
+                let newAddress = prompt("Enter new Address : ");
+                contact.address = newAddress;
+                break;
+            case '4':
+                let newCity = prompt("Enter new City : ");
+                contact.city = newCity;
+                break;
+            case '5':
+                let newState = prompt("Enter new State : ");
+                contact.state = newState;
+                break;
+            case '6':
+                let newZip = prompt("Enter new Zip :");
+                contact.zip = newZip;
+                break;
+            case '7':
+                let newPhno = prompt("Enter new Phone Number : ");
+                contact.phoneNo = newPhno;
+                break;
+            case '8':
+                let newEmail = prompt("Enter new Email : ");
+                contact.email = email;
+                break;
+            default:
+                console.log("Not a valid number. ");
+        }
+    }
+    return 1;
 }
